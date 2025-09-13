5bcfbf2cfe63d799f2fe3b529f2b992878f0d2b1
vinnu112p <patelvinnu112@gmail.com>
2025-09-13 14:45:31 +0530

    Update MaxAreaInHistogram.java

--- Stack/MaxAreaInHistogram.java
+++ Stack/MaxAreaInHistogram.java
@@ -1,68 +1,63 @@
-import java.util.Stack;
-public class MaxAreaInHistogram{
 
-    public static int largeArea(int height[]){
+import java.util.Stack;
+
+public class MaxAreaInHistogram {
+
+    public static int largeArea(int height[]) {
 
         Stack<Integer> st = new Stack<>();
         int maxArea = 0;
 
-
         int nsl[] = new int[height.length];   // Next smaller left  // 2,1,5,6,8,3
 
-        for(int i = 0; i < height.length; i++){
-            while(!st.isEmpty() && height[i] < height[st.peek()]){
+        for (int i = 0; i < height.length; i++) {
+            while (!st.isEmpty() && height[i] < height[st.peek()]) {
                 st.pop();
             }
 
-            if(st.isEmpty()){
+            if (st.isEmpty()) {
                 nsl[i] = -1;
-            }else{
+            } else {
                 nsl[i] = st.peek();
             }
             st.push(i);
         }
 
-
-
-        
-
-
+        st.clear();
 
         int nsr[] = new int[height.length];     // next smaller right
 
-        for(int i = height.length-1; i >= 0; i--){
+        for (int i = height.length - 1; i >= 0; i--) {
 
-            while(!st.isEmpty() && height[i] < height[st.peek()]){
+            while (!st.isEmpty() && height[i] < height[st.peek()]) {
                 st.pop();
             }
 
-            if(st.isEmpty()){
-                nsl[i] = height.length;
-            }else{
-                nsl[i] = height[st.peek()];
+            if (st.isEmpty()) {
+                nsr[i] = height.length;
+            } else {
+                nsr[i] = st.peek();
             }
 
             st.push(i);
 
         }
 
-        for(int i = 0; i < height.length; i++){
-            int currarea  = height[i]*(nsr[i]-nsl[i]-1);
-            maxArea = Math.max(currarea,maxArea);
+        for (int i = 0; i < height.length; i++) {
+            int currarea = height[i] * (nsr[i] - nsl[i] - 1);
+            maxArea = Math.max(currarea, maxArea);
         }
 
-    return maxArea;
-
+        return maxArea;
 
     }
 
     public static void main(String[] args) {
-        
-        int height[] = {2,1,5,6,8,3};
-        // int maxArea = Integer.MIN_VALUE;
-       
-        // int width = 1;
 
+        int height[] = {2, 1, 5, 6, 2, 3};
+        // int maxArea = Integer.MIN_VALUE;
+
+        // int width = 1;
         // for(int i = 0; i < height.length; i++){
         //      int min = height[i];
         //     for(int j = i; j< height.length; j++){
@@ -76,11 +71,8 @@ public class MaxAreaInHistogram{
         //     min = 0;
         //     width = 1;
         // }
-
         // System.out.println(maxArea);
-
         System.out.println(largeArea(height));
-
 
     }
 
